1643cfeb50b5d3b64053e1593a80a53e6907bd1a
wdhg <wdhgrant@gmail.com>
2021-11-16 11:58:45 +0000

    Refactor and test FIFO implementation

--- src/Simulation.hs
+++ src/Simulation.hs
@@ -4,25 +4,26 @@
 module Simulation where
 
 import Control.Monad.ST
+import Data.Array
 import Data.Array.ST
 import Data.STRef
 
 data FIFOCache :: * -> * where
   FIFOCache ::
-    { array :: STArray s Int Int,
+    { getArray :: STArray s Int Int,
       nextIndex :: STRef s Int
     } ->
     FIFOCache s
 
 cachedIn :: Int -> FIFOCache s -> ST s Bool
 cachedIn x cache = do
-  xs <- getElems $ array cache
+  xs <- getElems $ getArray cache
   return (x `elem` xs)
 
 getNextIndex :: FIFOCache s -> ST s Int
 getNextIndex cache = do
   index <- readSTRef $ nextIndex cache
-  (lowerBound, upperBound) <- getBounds $ array cache
+  (lowerBound, upperBound) <- getBounds $ getArray cache
   if index < upperBound
     then writeSTRef (nextIndex cache) (succ index)
     else writeSTRef (nextIndex cache) lowerBound
@@ -32,7 +33,7 @@ getNextIndex cache = do
 stash' :: Int -> FIFOCache s -> ST s ()
 stash' x cache = do
   index <- getNextIndex cache
-  writeArray (array cache) index x
+  writeArray (getArray cache) index x
   return ()
 
 stash :: Int -> FIFOCache s -> ST s ()
@@ -42,15 +43,25 @@ stash x cache = do
     then return ()
     else stash' x cache
 
+newFIFO :: Int -> ST s (FIFOCache s)
+newFIFO size = do
+  initArray <- newArray (0, size - 1) (-1) :: ST s (STArray s Int Int)
+  initNextIndex <- newSTRef 0
+  return $ FIFOCache initArray initNextIndex
+
+simulateFIFO' :: Int -> FIFOCache s -> ST s ()
+simulateFIFO' 0 cache = pure ()
+simulateFIFO' x cache = do
+  stash x cache
+  simulateFIFO' (x - 1) cache
+
 simulateFIFO :: Int -> IO ()
 simulateFIFO size = print $
-  runST $ do
-    initArray <- newArray (0, size - 1) (-1) :: ST s (STArray s Int Int)
-    initNextIndex <- newSTRef 0
-    let cache = FIFOCache initArray initNextIndex
-    stash 0 cache
-    stash 1 cache
-    return ()
+  elems $
+    runSTArray $ do
+      cache <- newFIFO size
+      simulateFIFO' 10 cache
+      return $ getArray cache
 
 simulate :: IO ()
-simulate = undefined
+simulate = simulateFIFO 10
