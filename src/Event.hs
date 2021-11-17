56c4d2b5a5d97d2359e6679c5673c666d35722d0
wdhg <wdhgrant@gmail.com>
2021-11-17 13:16:00 +0000

    Remove one leading 0 from Event log

--- src/Event.hs
+++ src/Event.hs
@@ -5,5 +5,5 @@ import Text.Printf
 data Event = Hit Int Float | Miss Int Float
 
 instance Show Event where
-  show (Hit id time) = "[" ++ printf "%014.5f" time ++ "] HIT  " ++ show id
-  show (Miss id time) = "[" ++ printf "%014.5f" time ++ "] MISS " ++ show id
+  show (Hit id time) = "[" ++ printf "%013.5f" time ++ "] HIT  " ++ show id
+  show (Miss id time) = "[" ++ printf "%013.5f" time ++ "] MISS " ++ show id
